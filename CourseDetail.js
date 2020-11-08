4f7a29f49fbab099579387c5860a32b2d2f85da7
wasirobin <69293514+wasirobin@users.noreply.github.com>
2020-11-09 07:25:14 +0800

    課程詳細頁

---
+++ CourseDetail.js
@@ -0,0 +1,37 @@
+CourseDetail.js
+
+
+import Navbar from '../component/Navbar'
+import ControlledCarousel from '../component/ControlledCarousel'
+import TopCourseDetail from '../component/TopCourseDetail'
+import Footer from '../component/Footer'
+import React, { useState, useEffect } from 'react'
+
+function CourseDetail() {
+  const [mydata, setMydata] = useState()
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+  return (
+    <>
+      <Navbar />
+      <ControlledCarousel />
+      <BodyCourseDetail />
+      <Footer />
+    </>
+  )
+}
+
+export default CourseDetail
