e58a6e0235867d5bc5085f5ad497f2c662cf1d12
wasirobin <69293514+wasirobin@users.noreply.github.com>
2020-11-09 07:15:51 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,28 @@
+App.js
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import CourseList from './pages/CourseList'
+import CourseDetail from './pages/CourseDetail'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './Navbar.css'
+import './CourseList.css'
+import './CourseDetail.css'
+
+function App() {
+  return (
+    <Router>
+      <>
+        <Switch>
+          <Route path="/CourseList">
+            <CourseList />
+          </Route>
+          <Route path="/CourseDetail">
+            <CourseDetail />
+          </Route>
+        </Switch>
+      </>
+    </Router>
+  )
+}
+
+export default App
